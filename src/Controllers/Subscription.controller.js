3dc3255f17ebde83be5d6c019e60131bbf69371d
Rishu Raj <rishuraj1127@gmail.com>
2024-12-08 01:00:58 +0530

    subscription controller

--- src/Controllers/Subscription.controller.js
+++ src/Controllers/Subscription.controller.js
@@ -3,6 +3,24 @@ import { ApiResponse } from "../Utils/ApiResponse";
 import { asyncHandler } from "../Utils/asynchandler";
 import { Subscription } from "../Models/Subscription.model";
 
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    // TODO: toggle subscription
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
 export {
-    
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
 }
