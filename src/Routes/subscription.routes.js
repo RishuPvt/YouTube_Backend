29b2279bc1ddb577b751f44b354b1fe6319c35dc
Rishu Raj <rishuraj1127@gmail.com>
2025-01-08 11:34:53 +0530

    susbcription route added

--- src/Routes/subscription.routes.js
+++ src/Routes/subscription.routes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { getSubscribedChannels , getUserChannelSubscribers ,toggleSubscription } from "../Controllers/Subscription.controller";
-import { verifyJWT } from "../Middleware/auth.middleware";
+import {subscribeToChannel,unsubscribeFromChannel, getSubscribedChannels , getUserChannelSubscribers ,getSubscriberCount } from "../Controllers/Subscription.controller.js";
+import { verifyJWT } from "../Middleware/auth.middleware.js";
 const router= Router()
+
+router.route("/subscribeToChannel").post(verifyJWT , subscribeToChannel)
+router.route("/unsubscribeFromChannel").post(verifyJWT , unsubscribeFromChannel)
+router.route("/getSubscribedChannels/:channelId").get(verifyJWT ,getSubscribedChannels )
+router.route("/getUserChannelSubscribers/:subscriberId").get(verifyJWT ,getUserChannelSubscribers )
+router.route("/getSubscriberCount/:channelId").get(verifyJWT ,getSubscriberCount)
 
 
 export default router;
